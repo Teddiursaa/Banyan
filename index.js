204be641b5198bef435a43476a2d03b84b5af226
Duc Anh Nguyen <nguyenducanh20066@gmail.com>
2024-09-17 20:28:15 +0200

    first release

--- index.js
+++ index.js
@@ -46,34 +46,50 @@ async function getEntity(TableClient, partitionKey, rowKey) {
 async function checkAuth(req) {
     const sessionID = req.sessionID;
     const session = req.session;
-    // console.log(sessionID);
-    // console.log(session);
-    // console.log(session.isAuth == undefined);
-    // console.log('check');
-    // console.log(await getEntity(Accounts, session.isAuth, session.isAuth).then((value) => { return value; }));
+    console.log('mim', sessionID);
     let notCookie = false;
     cookieStore.get(sessionID, (err, cookie) => {
         if (cookie == null || cookie == undefined) notCookie = true;
     });
-    // console.log(notCookie);
-    if (session.isAuth === null
+    console.log(session.isAuth);
+    if (notCookie
+     || session.isAuth == null
      || session.isAuth == undefined
-     || (await getEntity(Accounts, session.isAuth, session.isAuth).then((value) => { return value; }) === null) 
-     || notCookie) {
-        // console.log('good');
+     || (await getEntity(Accounts, session.isAuth, session.isAuth).then((value) => { return value; }) === null)
+        ) {
         return false;
     }
     return true;
 }
 
+app.use(async (req, res, next) => {
+    if (req.url.split('/')[1] == 'style') {
+        if (fs.existsSync(path.join(__dirname, req.url))) res.sendFile(path.join(__dirname, req.url));
+        return;
+    }
+    console.log(req.url);
+    console.log(req.url.split('/')[1]);
+    if (req.url.split('/')[1] == 'sendlogin') {
+        next();
+        return;
+    }
+    console.log("dak");
+    console.log(req.url);
+    if (req.url == '/login') {
+        console.log('asodj');
+        next();
+    }
+    else if (await checkAuth(req) === false) {
+        console.log("dak");
+        res.redirect('/login');
+        return;
+    }
+    else next();
+});
+
+
 app.get('/', async (req, res) => {
-    // console.log("abc " + await checkAuth(req));
-    if (await checkAuth(req) == true) {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    }
-    else {
-        res.send('Please login!~!');
-    }
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/login', async (req, res) => {
@@ -89,19 +105,22 @@ app.get('/login', async (req, res) => {
 
 import bcrypt from 'bcrypt';
 import { AppendBlobClient } from '@azure/storage-blob';
+import { start } from 'repl';
 
-app.post('/login', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.get('/sendlogin', async (req, res) => {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    console.log(username, password);
     const userID = await getEntity(AccountsID, username, username).then((value) => {
         if (value != null) return value.userID;
         return null;
     });
+
     console.log(username);
     console.log(password);
     console.log('userID ' + userID);
     if (userID == null) {
-        res.redirect('/login');
+        res.send({'good': false});
         res.end();
         return;
     }
@@ -112,24 +131,22 @@ app.post('/login', async (req, res) => {
         req.session.isAuth = userID;
         res.cookie("sessionId", req.sessionID);
         console.log('logged in!');
-        res.redirect('/');
+        res.send({'good': true});
+        res.end();
     }
     else {
-        res.redirect('/login');
+        res.send({'good': false});
+        res.end();
     }
 });
 
 app.get('/logout', (req, res) => {
     req.session.destroy();
-    res.sendFile(path.join(__dirname, 'public', 'logout.html'));
+    req.sessionID = undefined;
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 app.get('/api/appointments/:date', async (req, res) => {
-    if (await checkAuth(req) === false) {
-        res.redirect('/login');
-        res.end();
-        return;
-    }
     const ownerID = req.session.isAuth;
     console.log(ownerID);
     const ownerSettings = JSON.parse(await getEntity(Accounts, ownerID, ownerID).then((value) => {
@@ -144,7 +161,7 @@ app.get('/api/appointments/:date', async (req, res) => {
     console.log(beginTime);
     console.log(endTime);
     const workersList = Workers.listEntities({
-        queryOptions: { filter: odata`PartitionKey eq ${ownerID}` }
+        queryOptions: { filter: odata`PartitionKey eq ${ownerID} and active eq true` }
     });
 
     var answer = [];
@@ -179,41 +196,32 @@ app.get('/api/appointments/:date', async (req, res) => {
         "length": endTime - beginTime,
         "all": answer
     });
-    res.end();
 });
 
 app.get('/workers', async (req, res) => {
-    if (await checkAuth(req) === false) {
-        res.redirect('/login');
-        res.end();
-        return;
-    }
     res.sendFile(path.join(__dirname, 'public', 'workers.html'));
 });
 
 app.get('/api/workers', async (req, res) => {
-    if (await checkAuth(req) === false) {
-        res.redirect('/login');
-        res.end();
-        return;
-    }
     const ownerID = req.session.isAuth;
     console.log(ownerID);
     const workerList = Workers.listEntities({
-        queryOptions: { filter: odata`PartitionKey eq ${ownerID}` }
+        queryOptions: { filter: odata`PartitionKey eq ${ownerID} and active eq true` }
     });
     var currentWorker = [];
     for await (const worker of workerList) {
-        currentWorker.push({ "workerID": worker.rowKey, "name": worker.name });
+        console.log(worker.timestamp);
+        currentWorker.push({ "workerID": worker.rowKey, "name": worker.name, "timestamp": worker.timestamp });
     }
+    currentWorker.sort((a, b) => {
+        if (a.timestamp < b.timestamp) return -1;
+        return 1;
+    });
     res.send({"workers": currentWorker});
     res.end();
 });
 
 app.post('/new/appointments', async (req, res) => {
-    if (await checkAuth(req) === false) {
-        return;
-    }
     const ownerID = req.session.isAuth;
 
     console.log(req.headers);
@@ -232,17 +240,17 @@ app.post('/new/appointments', async (req, res) => {
         length: parseInt(req.headers.length)
     };
     console.log(Entity);
-    Appointments.upsertEntity(Entity, 'Merge');
+    await Appointments.upsertEntity(Entity, 'Merge');
+    res.end();
     return res;
 });
 
 app.post('/del/appointments', async (req, res) => {
-    if (await checkAuth(req) === false) {
-        return;
-    }
     const ownerID = req.session.isAuth;
 
+    console.log('chim chim chim chim chim');
     console.log(req.headers);
+    console.log('chim chim chim chim chim');
     const Entity = {
         partitionKey: req.headers.worker,
         rowKey: req.headers.start,
@@ -258,12 +266,132 @@ app.post('/del/appointments', async (req, res) => {
         length: 1
     };
     console.log(Entity);
-    Appointments.upsertEntity(Entity, 'Merge');
+    await Appointments.upsertEntity(Entity, 'Merge');
+    res.end();
     return res;
 });
 
-app.get('/workers/create', (req, res) => {
-    
+app.post('/new/workers', async (req, res) => {
+    const ownerID = req.session.isAuth;
+    const workerID = v4();
+    const worker = req.headers.worker;
+    console.log(ownerID);
+    const workerEntity = {
+        partitionKey: ownerID,
+        rowKey: workerID,
+        name: worker,
+        active: true,
+        deactivationDate: null
+    };
+    Workers.createEntity(workerEntity);
+    console.log(workerEntity);
+    res.end();
+    return res;
+});
+
+app.post('/del/workers', async (req, res) => {
+    const ownerID = req.session.isAuth;
+    const workerID = req.headers.workerid;
+    console.log(req.headers);
+    console.log(ownerID, workerID);
+    let Entity = await getEntity(Workers, ownerID, workerID);
+    console.log(Entity);
+    console.log(Entity.active, Entity.deactivationDate);
+    Entity.active = false;
+    Entity.deactivationDate = Date.now();
+    Workers.upsertEntity(Entity, 'Merge');
+    res.end();
+    return res;
+});
+
+app.get('/settings', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'settings.html'));
+});
+
+app.get('/api/settings', async (req, res) => {
+    const ownerID = req.session.isAuth;
+    const userlist = await AccountsID.listEntities({
+        queryOptions: { filter: odata`userID eq ${ownerID}` }
+    });
+    let username;
+    for await (const user of userlist) {
+        username = user.partitionKey;
+    }
+    console.log(username);
+
+    const Entity = await getEntity(Accounts, ownerID, ownerID);
+    console.log(Entity.settings);
+
+    const settings = JSON.parse(Entity.settings);
+
+    const startTime = settings.startTime;
+    const endTime = settings.endTime;
+
+    res.send({
+        "username": username,
+        "startTime": startTime,
+        "endTime": endTime
+    });
+    res.end();
+});
+
+app.get('/change/settings', async (req, res) => {
+    const userID = req.session.isAuth;
+    if (await getEntity(AccountsID, req.headers.username, req.headers.username).then((value) => {
+        if (value == null) return null;
+        return value.userID;
+    }) != userID) {
+        res.send({'good': false});
+        res.end();
+        return;
+    }
+    const oldpassword = req.headers.oldpassword;
+    const realPassword = await getEntity(Accounts, userID, userID).then((value) => {
+        return value.password;
+    });
+    if (!(await bcrypt.compare(oldpassword, realPassword))) {
+        res.send({'good': false});
+        res.end();
+        return;
+    }
+
+    console.log(req.headers);
+    const newpassword = req.headers.newpassword;
+    const startTime = req.headers.starttime;
+    const endTime = req.headers.endtime;
+    console.log(userID, startTime, endTime);
+    if (newpassword != "") {
+        let encryptedPassword;
+        bcrypt.genSalt(parseInt(process.env.SALTROUNDS), function (err, salt) {
+            if (err) console.log(err);
+            bcrypt.hash(newpassword, salt, function (err, hash) {
+                if (err) console.log(err);
+                console.log('hash', hash);  
+                encryptedPassword = hash;
+            });
+            return;
+        });
+        setTimeout(() => {
+            console.log(encryptedPassword);
+            Accounts.upsertEntity({
+                partitionKey: userID,
+                rowKey: userID,
+                password: encryptedPassword
+            }, 'Merge');
+        }, 1000);
+    }
+    Accounts.upsertEntity({
+        partitionKey: userID,
+        rowKey: userID,
+        settings: JSON.stringify({
+            "startTime": startTime,
+            "endTime": endTime  
+        })
+    }, 'Merge');
+    console.log('good');
+    res.send({'good': true});
+    res.end();
+    return;
 });
 
 const PORT = process.env.PORT || 3000;
